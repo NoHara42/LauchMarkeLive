dda5ca5da82411ed95f6f5d0b9c1c02e7329631c
Ned <nohaha@thinktoplinux.fritz.box>
2020-04-03 13:45:05 +0200

    init

---
+++ src/components/LauchLogo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <img class="responsiveLogo" src="../assets/LauchLogoTrimmed2.png"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  ScrollNext () {
+    alert('hello')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .responsiveLogo {
+        max-width: 55%;
+        height: auto;
+    }
+</style>
